14b430192169cdd73f05b591dbfdffc993ef3749
omelchenkoaleks <omelchenkoaleks@gmail.com>
2018-11-01 13:40:59 +0200

    Реализовали класс, который описывает таблицу базы данных.

--- app/src/main/java/com/omelchenkoaleks/addressbook/data/DatabaseDescription.java
+++ app/src/main/java/com/omelchenkoaleks/addressbook/data/DatabaseDescription.java
@@ -1,6 +1,8 @@
 package com.omelchenkoaleks.addressbook.data;
 
+import android.content.ContentUris;
 import android.net.Uri;
+import android.provider.BaseColumns;
 
 /**
  * Содержит описание таблицы contacts базы данных.
@@ -19,4 +21,31 @@ public class DatabaseDescription {
     // Базовый URI для взаимодействия с ContentProvider
     private static final Uri BASE_CONTENT_URI =
             Uri.parse("content://" + AUTHORITY);
+
+    // Вложенный класс, который будет определять содержимое таблицы contacts.
+    // Вместо того, чтобы определять константу _id (первичный ключ), идентифицирующий строку,
+    // мы реализуем интерфейс BaseColumns, который определяет константу _ID со значением "id".
+    public static final class Contact implements BaseColumns {
+
+        // Задаем имя таблицы.
+        public static final String TABLE_NAME = "contacts";
+
+        // объект Uri для обращения к таблице через ContentProvider
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon()
+                .appendPath(TABLE_NAME).build();
+
+        // Имена столбцов таблицы.
+        public static final String COLUMN_NAME = "name";
+        public static final String COLUMN_PHONE = "phone";
+        public static final String COLUMN_EMAIL = "email";
+        public static final String COLUMN_STREET = "street";
+        public static final String COLUMN_CITY = "city";
+        public static final String COLUMN_STATE = "state";
+        public static final String COLUMN_ZIP = "zip";
+
+        // Метод создает Uri для конктретного контакта.
+        public static Uri buildContactUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+    }
 }
